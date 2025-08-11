486dfd6000666da6b9418b43cd436251d1c74a0a
Maureen Osaghae <maureenosaghae86@gmail.com>
2025-08-11 22:04:07 +0100

    Delete Terraform Remote State File directory

--- Terraform Remote State File/imports.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = aws_iam_user.user2
-  id = "hgranger"
-}
--- Terraform Remote State File/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {
-  description = "The aws region to deploy in."
-  type        = string
-  default     = "us-east-1"
-}
